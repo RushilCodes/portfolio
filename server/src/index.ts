0226b2e4ba2e75ac6d2e2cbd1eed2b4e6461ad41
rushil gupta <rushilcodes@gmail.com>
2025-02-18 10:10:44 +0530

    .

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+Bun.serve({
+    port: 3000, // The port on which the server will listen
+    fetch(request) {
+      return new Response("Welcome to Bun!");
+    },
+  });
+  
+  console.log("Server is running on http://localhost:3000");
+  
